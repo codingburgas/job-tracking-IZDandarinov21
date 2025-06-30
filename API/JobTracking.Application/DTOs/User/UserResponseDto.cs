fb02a50a4a090fdce887fba284295d3d206402d7
IZDandarinov21 <IZDandarinov21@codingburgas.bg>
2025-07-01 00:25:05 +0300

    Add DTOs in project

---
+++ API/JobTracking.Application/DTOs/User/UserResponseDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobTracking.Application.DTOs.User
+{
+    internal class UserResponseDto
+    {
+    }
+}
